f2af37ec7d508f18c33094ede1c694c2a40f9bf6
tranqww <tranqwwdeveloper@gmail.com>
2026-08-20 00:20:56 +0300

    Made /add function

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import json
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
 from dotenv import load_dotenv
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -31,13 +34,18 @@ except FileNotFoundError as e:
     expenses = []
     with open(FILE_PATH, "w", encoding = "utf-8") as file:
         json.dump(expenses, file, ensure_ascii=False, indent=2)
-        print("File is created.")
+        print("File has been created.")
 except json.JSONDecodeError as e:
     print(f"JSON decode error: {e}")
     expenses = []
 
+class Form(StatesGroup):
+    category = State()
+    amount = State()
+
 bot = Bot(token=TOKEN)
-dp = Dispatcher()
+storage=MemoryStorage()
+dp = Dispatcher(storage=storage)
 
 @dp.message(Command("start"))
 #start command
@@ -60,6 +68,34 @@ async def cmd_list(message: types.Message):
     lines = [f"{item['category']}: {item['amount']}" for item in expenses]
     await message.answer("\n".join(lines))
 
+@dp.message(Command("add"))
+#add new element to expenses
+async def cmd_add(message: types.Message, state: FSMContext) -> None:
+    await message.answer("Input expense category:")
+    await state.set_state(Form.category)
+@dp.message(Form.category)
+#save category state
+async def state_category(message: types.Message, state: FSMContext) -> None:
+    await state.update_data(category=message.text)
+    await message.answer("Enter the amount of money spent on this expense:")
+    await state.set_state(Form.amount)
+@dp.message(Form.amount)
+async def state_amount(message: types.Message, state: FSMContext) -> None:
+    data = await state.get_data()
+    try:
+        await state.update_data(amount=float(message.text)) # type: ignore
+        data = await state.get_data()
+        print(data)
+    except ValueError:
+        await message.answer("Please, input an valid amount:")
+        return
+    global expenses  # noqa: PLW0602
+    expenses.append(data)
+    with open(FILE_PATH, "w", encoding = "utf-8") as file:  # noqa: ASYNC230
+        json.dump(expenses, file, ensure_ascii=False, indent=2)
+        print("File has been changed.")
+        await message.answer("Expense has been successfully added!")
+    await state.clear()
 
 async def main():
     await dp.start_polling(bot)
